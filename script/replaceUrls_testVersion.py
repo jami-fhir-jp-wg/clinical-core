9dd16c7a9a47ccd2dffb8fb90c62d9e9e0b090a0
Kazuhiko Ohe <kohe@m.u-tokyo.ac.jp>
2023-11-05 10:58:24 +0900

    replaceUrl　修正

--- script/replaceUrls_testVersion.py
+++ script/replaceUrls_testVersion.py
@@ -16,23 +16,24 @@ with open(sys.argv[1], "r") as f:
 # 次のようなパターンを見つけて、canonical= 値の部分だけに置き換える：https://simplifier.net/resolve?canonical=http%3A%2F%2Fjpfhir.jp%2Ffhir%2Fcore%2FValueSet%2FJP_MedicationSubstitutionNotAllowedReason_VS&scope=jpfhir-terminology@1.1.1
 
         if "https://simplifier.net/resolve?" in line:
-            #print(line, end="")
-            m1 = re.finditer('https:\/\/simplifier\.net\/resolve\?scope=(.*?)&.*?canonical=',line)
-            m2 = re.finditer('https:\/\/simplifier\.net\/resolve\?canonical=(.*?)&.*?scope=(.*?)"',line)
+            print(line, end="")
+            m1 = re.finditer('https:\/\/simplifier\.net\/resolve\?scope=(.*?)(&.*?canonical=)',line)
+            m2 = re.finditer('https:\/\/simplifier\.net\/resolve\?canonical=(.*?)(&.*?scope=.*?)"',line)
             if m1 :
                 for mm in m1:
-#                    print("mm1=",mm.group(1))
+                    print("mm1=",mm.group(1))
                     scope = mm.group(1)
-                    line = line.replace('https://simplifier.net/resolve?scope='+scope+'&canonical=','', 1)
+                    canonicalHeader = mm.group(2)
+                    line = line.replace('https://simplifier.net/resolve?scope='+scope + canonicalHeader,'', 1)
             elif m2 :
                 for mm in m2:
                     canonical = mm.group(1)
                     scope = mm.group(2)
-                    line = line.replace('https://simplifier.net/resolve?canonical='+canonical+'&amp;scope='+scope,canonical, 1)
+                    line = line.replace('https://simplifier.net/resolve?canonical='+canonical+scope,canonical, 1)
             else:
-                print(line, end="")
+                # print(line, end="")
                 continue
-            print(line, end="")
+            print("¥n------"+line)
         else:
-            print(line, end="")
+            # print(line, end="")
             continue
