d1d0562426b42cf25dc1994736569dd992d245db
Kazuhiko Ohe <kohe@m.u-tokyo.ac.jp>
2023-11-13 22:09:24 -0600

    package作成プロセスを改善（バージョンなど）

--- vali.sh
+++ vali.sh
@@ -3,7 +3,8 @@
 #テスト用
 java -Xmx4G -Djava.awt.headless=true -jar ../work/validator_cli_6.1.8.jar ExampleJson/*.json  -display-issues-are-warnings  -version 4.0.1  -language ja   -ig  jp-core.r4#1.1.2.tgz -ig jpfhir-terminology#1.1.1.tgz -ig jp-clins#1.0.0.tgz  -locale ja-JP -tx n/a 
 
-java -Xmx4G -Djava.awt.headless=true -jar ../work/validator_cli_6.1.8.jar ExampleJson/*.json  -display-issues-are-warnings  -version 4.0.1  -language ja   -ig  pkgValidation/jp-core.r4#1.1.2.tgz -ig pkgValidation/jpfhir-terminology#1.1.1.tgz -ig pkgValidation/jp-clins#1.0.0.tgz  -locale ja-JP -tx n/a 
+java -Xmx4G -Djava.awt.headless=true -jar ../work/validator_cli_6.1.8.jar ExampleJson/*.json  -display-issues-are-warnings  -version 4.0.1  -language ja    -ig pkgValidation/packages/hl7.fhir.r4.core#4.0.1  -ig  pkgValidation/jp-core.r4#1.1.2.tgz -ig pkgValidation/jpfhir-terminology#1.1.1.tgz -ig pkgValidation/jp-clins#1.0.0.tgz -locale ja-JP -tx n/a 
+
 
 # diff　パッケージ版
 java -Xmx4G -Djava.awt.headless=true -jar ../work/validator_cli_6.1.8.jar ExampleJson/*.json  -display-issues-are-warnings  -version 4.0.1  -language ja  -ig packagesForVali/jpfhir-terminology-1.1.1.tgz  -ig  packagesForVali/jp-core.r4-1.1.2tgz-ig packagesForVali/jp-clins-package-diff.tgz  -locale ja-JP -tx n/a 
