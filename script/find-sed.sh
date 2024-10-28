631e81adb70ddf6cb11dce58c5aa32c80801dcf6
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2024-10-29 05:43:45 +0900

    test3

--- script/find-sed.sh
+++ script/find-sed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec echo {} \;
 #find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' {} \;
-sed 's/x.x.x-profile/${{ env.release_version }}/g' input/fsh/profiles/extension/JP_eCS_OrgDept.fsh  > input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp
+sed "s/x.x.x-profile/$1/g" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh  > input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp
 mv input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
 echo '---0'
 grep "profile" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
@@ -9,7 +9,7 @@ grep '1.5.3' input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
 grep "1.5.3" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
 cat input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
 echo '---1'
-sed -ibak 's/x.x.x-profile/${{ env.release_version }}/g' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
+sed -ibak "s/x.x.x-profile/$1/g" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
 grep "profile" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
 grep '1.5.3' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
 ls -l input/fsh/profiles/extension/JP_eCS_DignosticType.fsh*
