fe1829762ccb011cabebfa3f51923ef2e63a28b4
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2024-10-29 06:05:08 +0900

    test6

--- script/find-sed.sh
+++ script/find-sed.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find input/fsh/profiles -type f -name '*.fsh' -exec sed -ibak "s/x.x.x-profile/$1/g" {} \;
-find input/fsh/profiles -type f -name '*.fsh' -exec grep -l $1 {} \;
+find input/fsh/profiles -type f -name '*.fsh' -exec grep $1 {} \;
+find input/fsh/profiles -type f -name '*.fshbak' -exec grep "x.x.x-profile" {} \;
