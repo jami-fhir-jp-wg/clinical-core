ba3fa29eb66519e00ceb8fe0e8fa80a6f7bc7f6f
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2024-10-29 05:59:50 +0900

    test5

--- script/find-sed.sh
+++ script/find-sed.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-filename
-
-#find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec echo {} \;
-#find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' {} \;
-sed "s/x.x.x-profile/$1/g" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh  > input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp
-mv input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
-echo '---0'
-grep "profile" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
-grep '1.5.3' input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
-grep "1.5.3" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
-cat input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
-echo '---1'
-sed -ibak "s/x.x.x-profile/$1/g" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
-cat input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
-grep "profile" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
-grep '1.5.3' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
-ls -l input/fsh/profiles/extension/JP_eCS_DignosticType.fsh*
-grep "profile" input/fsh/profiles/extension/JP_eCS_DignosticType.fshbak
-grep '1.5.3' input/fsh/profiles/extension/JP_eCS_DignosticType.fshbak
-echo '---2'
+find input/fsh/profiles -type f -name '*.fsh' -exec sed -ibak "s/x.x.x-profile/$1/g" {} \;
+find input/fsh/profiles -type f -name '*.fsh' -exec grep -l $1 {} \;
---
+++ script/find-sed2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec echo {} \;
+#find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' {} \;
+sed "s/x.x.x-profile/$1/g" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh  > input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp
+mv input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+echo '---0'
+grep "profile" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+grep '1.5.3' input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+grep "1.5.3" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+cat input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+echo '---1'
+sed -ibak "s/x.x.x-profile/$1/g" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
+cat input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
+grep "profile" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
+grep '1.5.3' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
+ls -l input/fsh/profiles/extension/JP_eCS_DignosticType.fsh*
+grep "profile" input/fsh/profiles/extension/JP_eCS_DignosticType.fshbak
+grep '1.5.3' input/fsh/profiles/extension/JP_eCS_DignosticType.fshbak
+echo '---2'
