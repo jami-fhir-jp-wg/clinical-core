6acc6dedaf1a954b91404b2736ba5ed05c6e134c
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2024-10-29 05:18:05 +0900

    -ie

--- script/find-sed.sh
+++ script/find-sed.sh
@@ -3,8 +3,10 @@
 #find sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' /home/runner/work/clinical-core/clinical-core/input/fsh/profiles -type f -name '*.fsh' -exec sed -i '' -e 's/x.x.x-profile/${{ env.release_version }}/g' {} \;
 sed 's/x.x.x-profile/${{ env.release_version }}/g' input/fsh/profiles/extension/JP_eCS_OrgDept.fsh  > input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp
 mv input/fsh/profiles/extension/JP_eCS_OrgDept.fsh_tmp input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+grep "profile" input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
+grep '${{ env.release_version }}' input/fsh/profiles/extension/JP_eCS_OrgDept.fsh
 echo '---1'
-sed -i '' 's/x.x.x-profile/${{ env.release_version }}/g' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
+sed -ibak 's/x.x.x-profile/${{ env.release_version }}/g' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
 grep "profile" input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
 grep '${{ env.release_version }}' input/fsh/profiles/extension/JP_eCS_DignosticType.fsh
 echo '---2'
