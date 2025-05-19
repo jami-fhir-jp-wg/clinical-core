56d35764950f8e8ba01940d5261fd770030d4c07
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2025-05-19 09:30:12 +0900

    1.4.0

--- pub.sh
+++ pub.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/.fhir/packages/'jp-core.r4#1.1.2'
 gtar xf packages_snapshot/jp-core.r4-1.1.2.tgz -C ~/.fhir/packages/'jp-core.r4#1.1.2'
 mkdir -p ~/.fhir/packages/'jpfhir-terminology.r4#1.3.0'
-gtar xf packages_snapshot/jpfhir-terminologyr4-current.tgz  -C ~/.fhir/packages/'jpfhir-terminology.r4#1.3.0'
+gtar xf packages_snapshot/jpfhir-terminologyr4-latest.tgz  -C ~/.fhir/packages/'jpfhir-terminology.r4#1.3.0'
 
 java -Xmx4G -Djava.awt.headless=true -jar ../work/publisher1.8.25.jar -ig ig.ini  -tx n/a
